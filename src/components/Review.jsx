a5f4c98ea405d92c5e374ee6098c136d4477dbc3
sifathossensuvo <sifathossensuvo@gmail.com>
2026-09-09 00:22:41 +0600

    fixing review issue

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -16,17 +16,17 @@ const Review = () => {
       id: 1,
       quote:
         "“Working with Younus was effortless from day one. He consistently turned ideas into thoughtful, polished designs, bringing clarity to every challenge and delivering results that exceeded our expectations.”",
-      name: "Kas Andz",
-      role: "Founder & CEO of KAMG",
-      image: user1,
+      name: "David Kauzlaric",
+      role: "Co-founder of Agency Elevation",
+      image: user2,
     },
     {
       id: 2,
       quote:
         "“Younus brought a thoughtful approach to every project. He understood our goals quickly, turned complex ideas into clear designs, and consistently delivered work that felt polished and purposeful.”",
-      name: "David Kauzlaric",
-      role: "Co-founder of Agency Elevation",
-      image: user2,
+      name: "Kas Andz",
+      role: "Founder & CEO of KAMG",
+      image: user1,
     },
     {
       id: 3,
