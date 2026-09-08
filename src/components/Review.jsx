3b6ed3a2b9780ed1b19cbf027a8bdcc164bdc658
sifathossensuvo <sifathossensuvo@gmail.com>
2026-09-09 00:20:55 +0600

    added real review

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -16,24 +16,24 @@ const Review = () => {
       id: 1,
       quote:
         "“Working with Younus was effortless from day one. He consistently turned ideas into thoughtful, polished designs, bringing clarity to every challenge and delivering results that exceeded our expectations.”",
-      name: "Natalie Brooks",
-      role: "Marketing Manager",
+      name: "Kas Andz",
+      role: "Founder & CEO of KAMG",
       image: user1,
     },
     {
       id: 2,
       quote:
-        "“Working with Younus was effortless from day one. He consistently turned ideas into thoughtful, polished designs, bringing clarity to every challenge and delivering results that exceeded our expectations.”",
-      name: "Alex Morgan",
-      role: "Product Designer",
+        "“Younus brought a thoughtful approach to every project. He understood our goals quickly, turned complex ideas into clear designs, and consistently delivered work that felt polished and purposeful.”",
+      name: "David Kauzlaric",
+      role: "Co-founder of Agency Elevation",
       image: user2,
     },
     {
       id: 3,
       quote:
-        "“Working with Younus was effortless from day one. He consistently turned ideas into thoughtful, polished designs, bringing clarity to every challenge and delivering results that exceeded our expectations.”",
-      name: "Sophia Chen",
-      role: "Founder & CEO",
+        "“An excellent team player who was always open to feedback and collaboration. He communicated clearly, supported the team when needed, and brought a positive, thoughtful attitude to every project.”",
+      name: "Ryan Battishill",
+      role: "Think Creative Founder",
       image: user3,
     },
   ];
